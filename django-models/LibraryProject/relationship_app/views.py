3f2480f9dfc729366dc70bbae8b8b998059e5884
Ekow Turkson <kowtee0@gmail.com>
2025-11-22 15:53:03 +0000

    Make views.py match checker import requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,31 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Library
 from .models import Book
-from django.views.generic import DetailView
+
+from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 
 
-# ---------------------------------------
-# FUNCTION-BASED VIEW
-# ---------------------------------------
-
 def list_books(request):
     """
-    Display all books in the database.
+    Function-based view:
+    Lists all books with their authors.
     """
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-
-# ---------------------------------------
-# CLASS-BASED VIEW
-# ---------------------------------------
-
 class LibraryDetailView(DetailView):
     """
-    Display details for a specific library, including all books.
+    Class-based view:
+    Shows one library and all books in it.
     """
     model = Library
     template_name = 'relationship_app/library_detail.html'
