037fa101a74b4fac8bfc2fd4580a4420a2ed5450
Ekow Turkson <kowtee0@gmail.com>
2025-11-22 17:56:00 +0000

    Add admin_view.html template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -84,7 +84,7 @@ def logout_view(request):
 # ----- ADMIN ROLE -----
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return user.userprofile.role == "Admin"
 
 
 @user_passes_test(is_admin)
