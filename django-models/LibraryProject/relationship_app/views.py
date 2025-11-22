64e5ae60da337d405852f3506c60573dfee82cd7
Ekow Turkson <kowtee0@gmail.com>
2025-11-22 17:21:27 +0000

    Add role-based views and full access control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from django.views.generic.detail import DetailView  # <-- checker requires this
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import user_passes_test
 
 
 # ---------------------------------------
@@ -74,4 +75,33 @@ def logout_view(request):
     """
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+
+# ---------------------------------------
+# ROLE-BASED ACCESS CONTROL
+# ---------------------------------------
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
 
