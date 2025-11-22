1594afa8d2e659000f4a05b3377b2e96685f192b
Ekow Turkson <kowtee0@gmail.com>
2025-11-22 15:26:42 +0000

    Fix paths and structure to satisfy checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,8 @@ def list_books(request):
     Display all books in the database.
     """
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 
 # ---------------------------------------
@@ -24,6 +25,6 @@ class LibraryDetailView(DetailView):
     Display details for a specific library, including all books.
     """
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
