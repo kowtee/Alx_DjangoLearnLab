040635fe82afcbf40c2d59478f78d843e0fc68c3
Ekow Turkson <kowtee0@gmail.com>
2025-12-07 20:59:38 +0000

    Add blog/login.html with CSRF token

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name="logout"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
